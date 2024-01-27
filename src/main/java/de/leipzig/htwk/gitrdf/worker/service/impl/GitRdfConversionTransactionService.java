ba48231bae6ec6379b0549cb817b0aab81983648
Superdose <72471357+Superdose@users.noreply.github.com>
2024-01-27 18:24:30 +0100

    Update to first full streaming solution

--- src/main/java/de/leipzig/htwk/gitrdf/worker/service/impl/GitRdfConversionTransactionService.java
+++ src/main/java/de/leipzig/htwk/gitrdf/worker/service/impl/GitRdfConversionTransactionService.java
@@ -4,18 +4,19 @@ import de.leipzig.htwk.gitrdf.worker.database.entity.GitRepositoryOrderEntity;
 import de.leipzig.htwk.gitrdf.worker.database.entity.enums.GitRepositoryOrderStatus;
 import de.leipzig.htwk.gitrdf.worker.database.entity.lob.GitRepositoryOrderEntityLobs;
 import jakarta.persistence.EntityManager;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.riot.system.StreamRDFWriter;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.hibernate.engine.jdbc.BlobProxy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,10 +33,19 @@ import java.util.zip.ZipInputStream;
 @Service
 public class GitRdfConversionTransactionService {
 
+    public static final String GIT_URI = "git://";
+    public static final String GIT_NAMESPACE = "git";
+    public static final String GIT_NAMESPACE_PREFIX = GIT_NAMESPACE + ":";
+
     private final EntityManager entityManager;
 
-    public GitRdfConversionTransactionService(EntityManager entityManager) {
+    private final int commitsPerIteration;
+
+    public GitRdfConversionTransactionService(
+            EntityManager entityManager, @Value("${worker.commits-per-iteration}") int commitsPerIteration) {
+
         this.entityManager = entityManager;
+        this.commitsPerIteration = commitsPerIteration;
     }
 
     @Transactional(rollbackFor = {SQLException.class, IOException.class, GitAPIException.class}) // Runtime-Exceptions are rollbacked by default; Checked-Exceptions not
@@ -145,52 +155,64 @@ public class GitRdfConversionTransactionService {
     //private void writeRdf(File gitFile, OutputStream targetOutputStream) throws GitAPIException, IOException {
     private InputStream writeRdf(File gitFile, GitRepositoryOrderEntityLobs entityLobs) throws GitAPIException, IOException {
 
-        Model rdfModel = ModelFactory.createDefaultModel();
-        String gitUri = "git://";
-        String gitNamespaceName = "git";
-
-        Property authorNameProperty = rdfModel.createProperty(gitUri + "AuthorName");
-        Property authorEmailProperty = rdfModel.createProperty(gitUri + "AuthorEmail");
-        Property authorDateProperty = rdfModel.createProperty(gitUri + "AuthorDate");
-        Property commitDateProperty = rdfModel.createProperty(gitUri + "CommitDate");
-        Property committerNameProperty = rdfModel.createProperty(gitUri + "CommitterName");
-        Property committerEmailProperty = rdfModel.createProperty(gitUri + "CommitterEmail");
-        Property commitMessageProperty = rdfModel.createProperty(gitUri + "CommitMessage");
-
-        // No possible solution found yet -> maybe traverse to parent? Maybe both
-        //Property mergeCommitProperty = rdfModel.createProperty(gitUri + "MergeCommit");
-
-        Repository gitRepository = new FileRepositoryBuilder().setGitDir(gitFile).build();
-
-        Git gitHandler = new Git(gitRepository);
-
-        Iterable<RevCommit> logs = gitHandler.log().call();
-
-        for (RevCommit commit : logs) {
-
-            Resource gitHash = rdfModel.createResource(gitUri + commit.getId().name());
-
-            gitHash.addProperty(authorNameProperty, commit.getAuthorIdent().getName());
-            gitHash.addProperty(authorEmailProperty, commit.getAuthorIdent().getEmailAddress());
-
-            Instant instant = Instant.ofEpochSecond(commit.getCommitTime());
-            LocalDateTime commitDateTime = instant.atZone(ZoneId.of("Europe/Berlin")).toLocalDateTime();
-
-            gitHash.addProperty(authorDateProperty, commitDateTime.toString());
-            gitHash.addProperty(commitDateProperty, commitDateTime.toString());
-            gitHash.addProperty(committerNameProperty, commit.getCommitterIdent().getName());
-            gitHash.addProperty(committerEmailProperty, commit.getCommitterIdent().getEmailAddress());
-            gitHash.addProperty(commitMessageProperty, commit.getFullMessage());
-
-            //commit.getParent()
-        }
-
-        rdfModel.setNsPrefix(gitNamespaceName, gitUri);
-
         File tempFile = Files.createTempFile("temp-rdf-write-file", ".dat").toFile();
 
         try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(tempFile))) {
-            RDFDataMgr.write(outputStream, rdfModel, Lang.TURTLE);
+
+            StreamRDF writer = StreamRDFWriter.getWriterStream(outputStream, RDFFormat.TURTLE_BLOCKS);
+            writer.prefix(GIT_NAMESPACE, GIT_URI);
+
+            Repository gitRepository = new FileRepositoryBuilder().setGitDir(gitFile).build();
+
+            Git gitHandler = new Git(gitRepository);
+
+            for (int iteration = 0; iteration < Integer.MAX_VALUE; iteration++) {
+
+                int skipCount = calculateSkipCountAndThrowExceptionIfIntegerOverflowIsImminent(iteration, commitsPerIteration);
+
+                Iterable<RevCommit> commits = gitHandler.log().setSkip(skipCount).setMaxCount(commitsPerIteration).call();
+
+                boolean finished = true;
+
+                writer.start();
+
+                for (RevCommit commit : commits) {
+
+                    finished = false;
+
+                    String gitHash = commit.getId().name();
+
+                    writer.triple(createAuthorNameProperty(gitHash, commit.getAuthorIdent().getName()));
+                    writer.triple(createAuthorEmailProperty(gitHash, commit.getAuthorIdent().getEmailAddress()));
+
+                    Instant instant = Instant.ofEpochSecond(commit.getCommitTime());
+                    LocalDateTime commitDateTime = instant.atZone(ZoneId.of("Europe/Berlin")).toLocalDateTime();
+
+                    writer.triple(createAuthorDateProperty(gitHash, commitDateTime.toString()));
+                    writer.triple(createCommitDateProperty(gitHash, commitDateTime.toString()));
+
+                    writer.triple(createCommitterNameProperty(gitHash, commit.getCommitterIdent().getName()));
+                    writer.triple(createCommitterEmailProperty(gitHash, commit.getCommitterIdent().getEmailAddress()));
+                    writer.triple(createCommitMessageProperty(gitHash, commit.getFullMessage()));
+
+                    // No possible solution found yet for merge commits -> maybe traverse to parent? Maybe both
+                    //Property mergeCommitProperty = rdfModel.createProperty(gitUri + "MergeCommit");
+                    //commit.getParent()
+                }
+
+                writer.finish();
+
+                if (finished) {
+                    break;
+                }
+
+                if (iteration + 1 == Integer.MAX_VALUE) {
+                    throw new RuntimeException(
+                            "While iterating through commit log and transforming log to rdf: Exceeded iteration max count (integer overflow)");
+                }
+
+            }
+
         }
 
         BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(tempFile));
@@ -237,5 +259,55 @@ public class GitRdfConversionTransactionService {
          */
     }
 
+    private int calculateSkipCountAndThrowExceptionIfIntegerOverflowIsImminent(
+            int currentIteration, int commitsPerIteration) {
+
+        long longCurrentIteration = (long) currentIteration;
+        long longCommitsPerIteration = (long) commitsPerIteration;
+
+        long skips = longCurrentIteration * longCommitsPerIteration;
+
+        if (skips > Integer.MAX_VALUE) {
+            throw new RuntimeException("While iterating through commit log and transforming log to rdf: Exceeded skip max count (integer overflow)");
+        }
+
+        return (int) skips;
+    }
+
+    private static Node literal(String value) {
+        return NodeFactory.createLiteral(value);
+    }
+
+    private static Node uri(String value) {
+        return NodeFactory.createURI(value);
+    }
+
+    private Triple createAuthorNameProperty(String gitHash, String authorNameValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "AuthorName"), literal(authorNameValue));
+    }
+
+    private Triple createAuthorEmailProperty(String gitHash, String authorEmailValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "AuthorEmail"), literal(authorEmailValue));
+    }
+
+    private Triple createAuthorDateProperty(String gitHash, String authorDateValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "AuthorDate"), literal(authorDateValue));
+    }
+
+    private Triple createCommitDateProperty(String gitHash, String commitDateValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "CommitDate"), literal(commitDateValue));
+    }
+
+    private Triple createCommitterNameProperty(String gitHash, String committerNameValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "CommitterName"), literal(committerNameValue));
+    }
+
+    private Triple createCommitterEmailProperty(String gitHash, String committerEmailValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "CommitterEmail"), literal(committerEmailValue));
+    }
+
+    private Triple createCommitMessageProperty(String gitHash, String commitMessageValue) {
+        return Triple.create(literal(gitHash), uri(GIT_URI + "CommitMessage"), literal(commitMessageValue));
+    }
 
 }
